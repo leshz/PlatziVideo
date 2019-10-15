8ee1cf77d23bbf12b41571581822613f2b2b04d0
Jeff Barragan <leshz@me.com>
2019-10-14 22:49:50 -0500

    404

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <React.Fragment>
+      <h1>No encontrado </h1>
+      <h2> Regresa a la pagina principal</h2>
+    </React.Fragment>
+  );
+};
+
+export default NotFound;
