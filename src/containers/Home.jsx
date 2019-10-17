8978b6ab5ce69d8d3981a66ae3de337f71fbe7a6
Jeff Barragan <leshz@me.com>
2019-10-16 21:03:25 -0500

    layput

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
@@ -14,7 +12,6 @@ const Home = () => {
   const initialState = useInitialState(API);
   return initialState.length === 0 ? <h1>Loading...</h1> : (
     <div className='App'>
-      <Header />
       <Search />
       {initialState.mylist.length > 0 && (
         <Categories title='Mi Lista'>
@@ -33,7 +30,6 @@ const Home = () => {
           {initialState.originals.map(item => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
-      <Footer />
     </div>
   );
 };
